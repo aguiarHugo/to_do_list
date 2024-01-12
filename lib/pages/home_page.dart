e4abda34189f569a2a459ccadf114132fb84f044
aguiarHugo <aguiarhugofx@gmail.com>
2024-01-12 09:42:12 -0300

    Created List, checkbox, dialog box and styled

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:to_do_list/utils/dialogbox.dart';
+import 'package:to_do_list/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //List of ToDo Tasks
+  List toDoList = [
+    ["Estudar", false],
+    ["Tomar Café", false],
+    ["Daily com o time", false],
+  ];
+
+  // checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  // create a new task
+  void createNewTask() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return DialogBox();
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          'TAREFAS',
+          style: TextStyle(
+            color: Colors.white, // Altere para a cor desejada
+          ),
+        )),
+        backgroundColor: Colors.purple[800],
+        elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      ),
+    );
   }
 }
