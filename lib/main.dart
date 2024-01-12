eaf8a876a2f6a91e0e09815ab90741f6a1f71b23
aguiarHugo <aguiarhugofx@gmail.com>
2024-01-12 08:03:34 -0300

    Renamed to HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_list/pages/todo_page.dart';
+import 'package:to_do_list/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ToDoPage(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
